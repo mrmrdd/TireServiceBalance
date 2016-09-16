0c58e6d256da495d0c0b850453c973f2f2493690
Artur <arturk.evgenievich@gmail.com>
2016-09-16 16:17:15 +0300

    fixed annotations and jsp pages

---
+++ src/main/java/com/ak/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.ak.config;
+
+/**
+ * Created by mrd on 12.09.2016.
+ */
+public class AppConfig {
+
+}
---
+++ src/main/java/com/ak/entities/enums/Roles.java
@@ -0,0 +1,14 @@
+package com.ak.entities.enums;
+
+/**
+ * Created by mrd on 12.09.2016.
+ */
+public enum Roles {
+   ANONYMUS,
+    ADMIN,
+    CLIENT;
+
+    Roles(){
+
+    }
+}
